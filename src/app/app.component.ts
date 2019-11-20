625f64dc49a6bd0e0132e13765aab38a267deade
shadowgun6 <shadowgun625@gmail.com>
2019-11-20 11:09:23 -0800

    login created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Course } from 'src/app/models/course.model';
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+import './_content/app.less';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'Attendance-Team';
-  courseList: Course[] = new Array(10);
-  constructor() {
-    for (var i = 0; i < 10; i++) {
-      this.courseList[i] = this.fillCourse(i);
-    }
-    console.log(this.courseList[0]);
-  }
+    currentUser: User;
 
-  fillCourse(i: number) {
-    var aCourse: Course = new Course();
-    aCourse = {
-      name: 'CLASS ' + i + 1,
-      professor: 'Dr. Spaceman',
-      section: (i + 1) * 1402,
-      room: 'RM ' + 1321*(i+1)%10000,
-      days: [
-        'Mo',
-        'We'
-      ],
-      times: [
-        '9:30a',
-        '10:45a'
-      ]
-    };
-    return aCourse;
-  }
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
 }
