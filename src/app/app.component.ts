b7465841c40208c100a86ab93d6cd1fb4c95d27f
zamorapiero <47881531+zamorapiero@users.noreply.github.com>
2019-11-25 14:05:01 -0800

    Student Page Changes
    Student Page Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './student.page.html',
+  styleUrls: ['./student.page.css']
 })
 export class AppComponent {
-  title = 'Attendance';
+  title = 'Attendance-Team';
+  courseList: Course[] = new Array(10);
+  constructor() {
+    for (var i = 0; i < 10; i++) {
+      this.courseList[i] = this.fillCourse(i);
+    }
+    console.log(this.courseList[0]);
+  }
+
+  fillCourse(i: number) {
+    var aCourse: Course = new Course();
+    aCourse = {
+      name: 'CLASS ' + i + 1,
+      professor: 'Dr. Spaceman',
+      section: (i + 1) * 1402,
+      room: 'RM ' + 1321*(i+1)%10000,
+      days: [
+        'Mo',
+        'We'
+      ],
+      times: [
+        '9:30a',
+        '10:45a'
+      ]
+    };
+    return aCourse;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './course-info/course-info.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { CardLayoutComponent } from './card-layout/card-layout.component';
+import { CardOverviewExample } from './card-overview-example/card-overview-example'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseInfoComponent,
+    NavBarComponent,
+    CourseCardComponent,
+    CardLayoutComponent,
+    CardOverviewExample
   ],
   imports: [
     BrowserModule,
