2a76ecc612849ae6687163d8f17f7b6d77ad3a82
Carlos <guerreroc47@gmail.com>
2020-05-03 01:51:28 -0700

    installed npm and added the path from import

--- src/app/student.model.spec.ts
+++ src/app/student.model.spec.ts
@@ -1,7 +1,7 @@
-import { Student } from './student.model';
-
-describe('Student', () => {
-  it('should create an instance', () => {
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Student } from "./student.model";
+describe("Student", () => {
+  it("should create an instance", () => {
     expect(new Student()).toBeTruthy();
   });
 });
