d8455660c0ddf29c62e320baac44a18a923616d7
Carlos <guerreroc47@gmail.com>
2020-05-03 01:50:55 -0700

    created CRUD

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
-
+import { Student } from "src/app/student.model";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Injectable } from "@angular/core";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StudentService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  // method to get retrieve the available from Firestore collection
+  getStudent() {
+    return this.firestore.collection("student").snapshotChanges();
+  }
+  // method to persist a student in the firestore database
+  createStudent(student: Student) {
+    return this.firestore.collection("student").add(student);
+  }
+  // method to update an student by its student
+  updateStudent(student: Student) {
+    delete student.name;
+    this.firestore.doc("student/" + student.name).update(student);
+  }
+  // method to delete a student by its student
+  deleteStudent(studentId: string) {
+    this.firestore.doc(" student / " + studentId).delete();
+  }
 }
